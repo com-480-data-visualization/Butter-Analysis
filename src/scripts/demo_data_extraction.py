b60d9783ee6050e8bb079ed1f442b4c5eebb1b0b
ElMaestroZ <robin.herberich1@gmail.com>
2026-03-07 16:27:52 +0100

    feat: improved script

--- src/scripts/demo_data_extraction.py
+++ src/scripts/demo_data_extraction.py
@@ -11,32 +11,38 @@ dataset_path = os.path.join(base_dir, 'datasets', 'dairy_consumer_prices', 'dair
 # Load the dataset
 df = pd.read_csv(dataset_path)
 
-# Filter for Gruyère products
-gruyere_df = df[df['product_category'] == 'Gruyère']
+product = 'Mozzarella'  # Change this to the desired product category
+
+# Filter for products
+product_df = df[df['product_category'] == product]
+unit = product_df['unit_name'].unique()[0]  # Assuming all entries for the product have the same unit
 
 # Convert 'month' to datetime objects
-gruyere_df['month'] = pd.to_datetime(gruyere_df['month'])
+product_df['month'] = pd.to_datetime(product_df['month'])
 
 # Calculate the average price for each month
-average_price_per_month = gruyere_df.groupby(['month', 'exact_product'])['price_value'].mean().reset_index()
+average_price_per_month = product_df.groupby(['month', 'exact_product', 'production_type'])['price_value'].mean().reset_index()
 
 # Plotting
 plt.figure(figsize=(12, 6))
-for product in average_price_per_month['exact_product'].unique():
-    product_df = average_price_per_month[average_price_per_month['exact_product'] == product]
-    plt.plot(product_df['month'], product_df['price_value'], marker='o', label={product if product != 'Gruyère' else 'Gruyère Bio'})
-plt.title('Average Price of Gruyère Over Time')
+# Get unique combinations of exact_product and production_type
+for (exact_product, production_type), group in average_price_per_month.groupby(['exact_product', 'production_type']):
+    label = f'{exact_product}'
+    if production_type == 'Bio':
+        label += ' Bio'
+    plt.plot(group['month'], group['price_value'], marker='o', label=label)
+plt.title(f'Average Price of {product} Over Time')
 plt.xlabel('Month')
-plt.ylabel('Average Price (CHF per kg)')
+plt.ylabel(f'Average Price (CHF per {unit})')
 plt.legend()
 plt.grid(True)
 plt.xticks(rotation=45)
 plt.tight_layout()
 
 # Save the plot
-output_dir = os.path.join(base_dir, 'public')
+output_dir = os.path.join(base_dir, 'charts')
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-plt.savefig(os.path.join(output_dir, 'gruyere_price_chart.png'))
+plt.savefig(os.path.join(output_dir, f'{product}_price_chart.png'))
 
-print(f"Plot saved to {os.path.join(output_dir, 'gruyere_price_chart.png')}")
+print(f"Plot saved to {os.path.join(output_dir, f'{product}_price_chart.png')}")
