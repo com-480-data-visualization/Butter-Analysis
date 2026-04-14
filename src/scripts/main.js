6fa787ed52977dc27f7c0c770bdb2efde1b18be2
ElMaestroZ <robin.herberich1@gmail.com>
2026-04-14 20:27:34 +0200

    feat: basic navbar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,3 +5,10 @@ document.querySelector('#app').innerHTML = `
     Hello world!
     </h1>
 `
+
+const hamburgerButton = document.getElementById('hamburger-button');
+const navMenu = document.getElementById('nav-menu');
+
+hamburgerButton.addEventListener('click', () => {
+  navMenu.classList.toggle('hidden');
+});
